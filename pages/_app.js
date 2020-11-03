8203c15a5e4273c0bf34b53a03f69d9f43e571b3
Brad Purchase <bradpurchase@gmail.com>
2020-11-02 20:12:55 -0500

    only render script client side

--- pages/_app.js
+++ pages/_app.js
@@ -19,14 +19,15 @@ function App({ Component, pageProps }) {
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
-          {process.env.NODE_ENV === "production" && (
-            <script
-              async
-              defer
-              data-website-id="fe82d042-84f4-44c3-8359-d809bd599144"
-              src="https://umami-8j4la46es.vercel.app/umami.js"
-            />
-          )}
+          {typeof window !== "undefined" &&
+            process.env.NODE_ENV === "production" && (
+              <script
+                async
+                defer
+                data-website-id="fe82d042-84f4-44c3-8359-d809bd599144"
+                src="https://umami-8j4la46es.vercel.app/umami.js"
+              />
+            )}
         </Head>
         <Component {...pageProps} />
         <Footer />
