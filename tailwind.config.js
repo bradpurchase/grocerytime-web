c4a0d2a0796968870bc167eecc123f2bff30ec89
Brad Purchase <bradpurchase@gmail.com>
2020-11-19 21:37:54 -0500

    specify files to purge in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./components/**/*.tsx", "./pages/**/*.js", "./pages/**/*.tsx"],
   theme: {
     extend: {
       screens: {
